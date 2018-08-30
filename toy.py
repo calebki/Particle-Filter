2d2fa527170a44a1da81ee896820a101a5aaed5a
Jonathan Terhorst <terhorst@gmail.com>
2018-08-30 10:00:42 -0400

    refactor and fix algorithm

--- toy.py
+++ toy.py
@@ -1,40 +1,32 @@
-#Simple nonlinear time series model
+# Simple nonlinear time series model
 import numpy as np
 import math
-global sigma1
-sigma1 = math.sqrt(10)
-global sigma2
-sigma2 = 1
+import scipy.stats
 
-def latent(t, prev, n):
-    curr = prev/2 + 25*prev/(1+prev**2) + 8*math.cos(1.2*t) \
-         + np.random.normal(scale=sigma1, size = n)
-    return curr
+SIGMA_U = math.sqrt(10)
+SIGMA_V = 1
 
-def obs(x, n):
-    y = x**2/20 + np.random.normal(scale=sigma2, size = n)
-    return y
 
-def init(n):
-    return np.random.normal(scale=math.sqrt(10), size = n)
+class Example1:
 
-def init_lik(x):
-    lik = 1/((2*math.pi)*10)**(1/2) * np.exp(
-    -1* x**2 / (20))
-    return lik
+    def __init__(self, n):
+        self.xt = self.initial_dist().rvs(size=n)
+        self._t = 1
 
-def obs_lik(y, x):
-    lik = 1/((2*math.pi)*sigma2)**(1/2) * np.exp(
-    -1*(y-x**2/20)**2 / (2*sigma2**2))
-    return lik
+    def initial_dist(self):
+        return scipy.stats.norm(loc=0, scale=SIGMA_V)
 
-def trans_lik(t, x, prev):
-    lik = 1/(2*math.pi*sigma1)**(1/2) * np.exp(
-    -1*(x-(prev/2 + 25*prev/(1+prev**2) + 8*math.cos(1.2*t)))**2 / (2*sigma1**2))
-    return lik
+    def latent_dist(self, t, xt1):
+        "Distribution of x_t conditional on x_{t-1}, t"
+        mu = xt1 / 2 + 25 * xt1 / (1 + xt1 ** 2) + 8 * math.cos(1.2 * t)
+        return scipy.stats.norm(loc=mu, scale=SIGMA_U)
 
-def prop_init_lik(x ,y):
-    return init_lik(x)
+    def obs_dist(self, xt):
+        return scipy.stats.norm(loc=xt ** 2 / 20, scale=SIGMA_V)
 
-def prop_lik(t, x, prev, y):
-    return trans_lik(t, x, prev)
+    def sample(self):
+        "Given current state x_{t-1}, sample a pair (x_t, y_t)"
+        self.xt = self.latent_dist(self._t, self.xt).rvs()
+        yt = self.obs_dist(self.xt).rvs()
+        self._t += 1
+        return np.array((self.xt, yt))
